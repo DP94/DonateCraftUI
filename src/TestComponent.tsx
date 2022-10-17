2c4496f6c072a10753298bfabf0ec67514dc3d05
DP94 <d.pell1994@gmail.com>
2022-10-17 20:28:50 +0100

    First commit

---
+++ src/TestComponent.tsx
@@ -0,0 +1,21 @@
+﻿import React, {useState} from "react";
+
+class TestComponent extends React.Component {
+    
+    render() {
+        return (
+            <h1>test</h1>
+        )
+    }
+    
+    componentDidMount() {
+        fetch('https://jr0me1l0t9.execute-api.eu-west-2.amazonaws.com/Prod/api/values').then(async response => {
+            const data = await response.json();
+            return data[0];
+        }).catch(error => {
+            console.error(error)
+        })
+    }
+}
+
+export default TestComponent;
