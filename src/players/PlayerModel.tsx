53a0eabf10a077039f58520f6802667195038521
VTVDV <veronica.vandevyver@gmail.com>
2022-10-26 22:12:35 +0100

    Making start on player model and adding in configs for Jest and Babel.

---
+++ src/players/PlayerModel.tsx
@@ -0,0 +1,9 @@
+﻿export class PlayerModel {
+    id : string;
+    name : string;      
+    
+    constructor(id: string, name: string) {
+        this.id = id;
+        this.name = name;
+    }
+}
