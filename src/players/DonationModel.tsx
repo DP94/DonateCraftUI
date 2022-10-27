99538e06611a941cecbe747cf7cf2533d01b1aef
VTVDV <veronica.vandevyver@gmail.com>
2022-10-27 21:01:08 +0100

    Fleshed out player and related models

---
+++ src/players/DonationModel.tsx
@@ -0,0 +1,26 @@
+﻿import {CharityModel} from "./CharityModel";
+import {PlayerModel} from "./PlayerModel";
+
+export class DonationModel{
+    
+    id: string;
+    amount: number;
+    createdDate: Date;
+    charity: CharityModel;
+    paidForId: string;
+    paidForBy: PlayerModel
+    
+    constructor(id: string,
+    amount: number,
+    createdDate: Date,
+    charity: CharityModel,
+    paidForId: string,
+    paidForBy: PlayerModel) {
+        this.id = id;
+        this.amount = amount;
+        this.createdDate = createdDate;
+        this.charity = charity;
+        this.paidForId = paidForId;
+        this.paidForBy = paidForBy;
+    }
+}
--- src/players/PlayerModel.tsx
+++ src/players/PlayerModel.tsx
@@ -1,9 +1,16 @@
-﻿export class PlayerModel {
+﻿import {DeathModel} from "./DeathModel";
+import {DonationModel} from "./DonationModel";
+
+export class PlayerModel {
     id : string;
-    name : string;      
+    name : string; 
+    deaths: DeathModel[];
+    donations: DonationModel[]
     
-    constructor(id: string, name: string) {
+    constructor(id: string, name: string, deaths: DeathModel[], donations: DonationModel[]) {
         this.id = id;
         this.name = name;
+        this.deaths = deaths;
+        this.donations = donations;
     }
 }
