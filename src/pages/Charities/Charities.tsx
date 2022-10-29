98b60fd351182701ae270f9cf7484d156d15a7da
DP94 <d.pell1994@gmail.com>
2022-10-30 00:10:56 +0100

    Implement charities page

--- src/pages/Charities/Charities.tsx
+++ src/pages/Charities/Charities.tsx
@@ -1,10 +1,70 @@
 ﻿import React from "react";
+import CharityService from "./charity-service";
+import Charity from "./charity";
+import './Charity.css';
 
-class Charities extends React.Component {
+class Charities extends React.Component<{}, {charities: Charity[]}> {
+    
+    charityService = new CharityService();
+    
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            charities: []
+        }
+    }
+    
+    async componentDidMount() {
+        const charityIds: Charity[] = await this.charityService.getCharityIds();
+        for (let i = 0; i < charityIds.length; i++) {
+            const charityId = charityIds[i].id;
+            const charityDetails = await this.charityService.getCharityDetails(charityId);
+            this.setState( {
+                charities : [...this.state.charities, charityDetails]
+            });
+        }
+    }
+    
+    charityDonateClicked(charity: Charity) {
+        let url = `${process.env.REACT_APP_JG_DONATE_URL}${charity.id}?exiturl=${process.env.REACT_APP_API_URL}v1/Callback?data=JUSTGIVING-DONATION-ID~5ba92742-af9d-4ad6-a5a7-c768dd9bc747`;
+        window.open(url);
+    }
+
     render() {
-        return (
-            <h1>Charities</h1>
-        )
+        if (this.state && this.state.charities.length !== 0) {
+            return (
+                <div>
+                    <h1>Charities</h1>
+                    <table className="charity-table table-striped table table-hover table-responsive">
+                        <thead className="table-light">
+                            <tr>
+                                <th></th>
+                                <th>Name</th>
+                                <th></th>
+                                <th>Description</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.charities.map((charity) => (
+                                    <tr className="charity-row">
+                                        <td className="charity-table-data"><img className="charity-image"
+                                                                                src={charity.logoAbsoluteUrl}/></td>
+                                        <td className="charity-table-data"><p>{charity.name}</p></td>
+                                        <td className="charity-table-data">
+                                            <button className="btn btn-success charity-button" onClick={() => this.charityDonateClicked(charity)}>Donate</button>
+                                        </td>
+                                        <td className="charity-table-data"><p>{charity.description}</p></td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            )
+        } else {
+            return <span>Loading...</span>
+        }
     }
 }
 
---
+++ src/pages/Charities/charity-service.ts
@@ -0,0 +1,19 @@
+﻿import Charity from "./charity";
+
+class CharityService {
+    
+    async getCharityIds(): Promise<Charity[]> {
+        const result = await fetch(`${process.env.REACT_APP_API_URL}v1/Charity/`);
+        return await result.json();
+    }
+    
+    async getCharityDetails(id: number): Promise<Charity> {
+        const requestOptions = {
+            method: 'GET',
+            headers: { 'Accept': 'application/json' }
+        }
+        const result = await fetch(`${process.env.REACT_APP_JG_API_URL}${process.env.REACT_APP_JG_API_KEY}/v1/charity/${id}`, requestOptions)
+        return await result.json();
+    }
+}
+export default CharityService;
