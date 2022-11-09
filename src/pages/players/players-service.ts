9a7ebcfdab5701c044e5bb2cd3eb5e401c7efb0e
DP94 <d.pell1994@gmail.com>
2022-11-09 22:28:42 +0000

    Add sortOrder=desc now server supports better sorting

--- src/pages/players/players-service.ts
+++ src/pages/players/players-service.ts
@@ -3,7 +3,7 @@
 class PlayersService {
     async getPlayers(): Promise<Player[]> {
         try {
-            const result = await fetch(`${process.env.REACT_APP_API_URL}v1/Player?sortBy=deaths`)
+            const result = await fetch(`${process.env.REACT_APP_API_URL}v1/Player?sortBy=deaths&sortOrder=desc`)
             return await result.json();
         } catch (e) {
             console.error(e);
